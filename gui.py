7fb053d3886500464d18dd719da1c3485da03383
andrei <andrew.skirchenko@gmail.com>
2023-05-25 00:52:32 +0300

    filesystem info added

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtCore import Qt
 
 from loguru import logger
 
@@ -15,6 +16,21 @@ class MainGui(QtWidgets.QWidget, main_form.Ui_Form):
 
         self.init_signals()
 
+    @staticmethod
+    def _get_txt_progress_widget(text, progress_value):
+        disk_usage_progress = QtWidgets.QProgressBar()
+        disk_usage_progress.setValue(progress_value)
+        disk_usage_label = QtWidgets.QLabel()
+        disk_usage_label.setText(text)
+        disk_usage_layout = QtWidgets.QHBoxLayout()
+        disk_usage_widget = QtWidgets.QWidget()
+        disk_usage_layout.addWidget(disk_usage_label)
+        disk_usage_layout.addWidget(disk_usage_progress)
+        disk_usage_layout.setAlignment(Qt.AlignRight)
+        disk_usage_widget.setLayout(disk_usage_layout)
+
+        return disk_usage_widget
+
     def init_signals(self):
         self.tabWidget.currentChanged.connect(self.send_tab_number)
 
@@ -28,5 +44,18 @@ class MainGui(QtWidgets.QWidget, main_form.Ui_Form):
         logger.debug(f'Sys info received: {info_string}')
         self.info.setPlainText(info_string)
 
-    def show_fs_info(self, info):
-        logger.debug(info)
+    def show_fs_info(self, fs_info):
+        logger.debug(fs_info)
+        self.fs_table.setRowCount(len(fs_info))
+
+        for i, device in enumerate(fs_info):
+            for j, item in enumerate(device):
+                self.fs_table.setItem(i, j, QtWidgets.QTableWidgetItem(str(item)))
+
+                if j == 5:
+                    self.fs_table.setCellWidget(i, j, self._get_txt_progress_widget(str(item), int(device.percent)))
+
+        self.fs_table.resizeColumnsToContents()
+        self.fs_table.resizeRowsToContents()
+
+
